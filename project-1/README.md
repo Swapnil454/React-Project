0bc81ced27ed13aea04e51804284961e91c9d76a
Swapnil454 <shivamshelke39@gmail.com>
2025-08-01 08:45:11 +0530

    project-1

---
+++ project-1/README.md
@@ -0,0 +1,14 @@
+# React Router Project
+
+A scalable React project built with Vite, React Router, and advanced architecture for real-world applications. Focused on performance, clean code, and future-proof structure.
+
+## Tech Stack
+- React + Vite
+- React Router
+- HMR (Hot Module Replacement)
+- ESLint
+
+## Development
+This project uses Vite with the following official plugins:
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ project-1/src/components/Github/Github.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
+    
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github User Name: {data.login} <br /> Link : {data.url} <br /> Follow : {data.following_url}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    /*const response = await fetch('https://api.github.com/users/hiteshchoudhary')*/
+    const response = await fetch('https://api.github.com/users/Swapnil454')
+    return response.json()
+}
+
